62abdf6e6c44a7cd3de4b5dbf6ae3baee744e6ab
姚烽 <yaofeng@fengwall.com>
2019-08-16 13:38:26 +0800

    1

--- sample/net45/Overt.GrpcExampleNet45.Service.Grpc/ClientManager.cs
+++ sample/net45/Overt.GrpcExampleNet45.Service.Grpc/ClientManager.cs
@@ -8,32 +8,23 @@ using __GrpcService = Overt.GrpcExample.Service.Grpc.GrpcExampleService;
 namespace Overt.GrpcExample.Service.Grpc
 {
 #if NET45 || NET46 || NET47
-    public class ClientManager
-    {
-        public static IClientTracer Tracer { get; set; } = default(IClientTracer);
+public class ClientManager {
+public static IClientTracer Tracer { get; set; } = default(IClientTracer);
         private static string DefaultConfigPath { get; set; } = "dllconfigs/Overt.GrpcExample.Service.Grpc.dll.config";
-        public static __GrpcService.GrpcExampleServiceClient Instance
-        {
-            get
-            {
-                return ClientManager<__GrpcService.GrpcExampleServiceClient>.Instance;
-            }
-        }
-        private static readonly ConcurrentDictionary<Type, string> configMap = new ConcurrentDictionary<Type, string>();
-        public static void Configure<T>(string configPath) { configMap.AddOrUpdate(typeof(T), configPath, (t, s) => configPath); }
-        public static string GetConfigure<T>() { if (configMap.TryGetValue(typeof(T), out string configPath)) return configPath; return DefaultConfigPath; }
-    }
-    public class ClientManager<T> : ClientManager where T : ClientBase
-    {
-        public static new T Instance
-        {
-            get
-            {
-                var configPath = GetConfigure<T>();
-                var abConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configPath);
-                return GrpcClientManager<T>.Get(abConfigPath, Tracer);
-            }
-        }
-    }
+public static __GrpcService.GrpcExampleServiceClient Instance{get{
+return ClientManager<__GrpcService.GrpcExampleServiceClient>.Instance;
+} }
+private static readonly ConcurrentDictionary<Type, string> configMap = new ConcurrentDictionary<Type, string>();
+public static void Configure<T>(string configPath) { configMap.AddOrUpdate(typeof(T), configPath, (t, s) => configPath); }
+public static string GetConfigure<T>() { if (configMap.TryGetValue(typeof(T), out string configPath)) return configPath; return DefaultConfigPath;}
+}
+public class ClientManager<T> : ClientManager where T : ClientBase
+{
+public static new T Instance{ get {
+var configPath = GetConfigure<T>();
+var abConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configPath);
+return GrpcClientManager<T>.Get(abConfigPath, Tracer);
+} }
+}
 #endif
 }
